8528ee8da663b73762f1046bb60b722ca0e53431
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-12-04 16:49:12 +0300

    Добавил пользователей с правам и контроль доступа.
    Сначала надо подготовить таблицы в базе данных.
    
    Создать таблицу с пользователями:
    create table users (username varchar(50) primary key, password varchar(100) not null, enabled boolean not null);
    
    Роль привязывается к таблице users, по этому мы будем использовать привязку username в таблице authorities в качестве внешнего ключа к username в таблице users. Будем ссылаться username в таблице authorities на username в таблице users.
    create table authorities (username varchar(50) not null, authority varchar(50) not null, foreign key (username) references users (username));
    
    Затем создаем юзера
    insert into users (username, password, enabled) values ('user', '{noop}userpass', true);
    insert into users (username, password, enabled) values ('admin', '{noop}adminpass', true);
    
    Добавим им роли
    insert into authorities (username, authority) values ('user', 'ROLE_USER');
    insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
    
    Затем для примера создадим по 1 пользователю user и admin.

--- src/main/java/development/bulletinboard/configuration/SecurityConfiguration.java
+++ src/main/java/development/bulletinboard/configuration/SecurityConfiguration.java
@@ -1,42 +1,53 @@
 package development.bulletinboard.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * Атентификация будет через jdbc и базу данных, а базу данных возьмем из dataSource,
+     * прописанного в application.properties
+     * @param auth аутентификация
+     * @throws Exception если что не так
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    /**
+     * Настройки доступа по HTTP для всех типов пользователей.
+     * @param config - конфигурация HTTP доступа
+     * @throws Exception - если что не так
+     */
     @Override
     public void configure(HttpSecurity config) throws Exception {
         config
                 .authorizeRequests()
-                .antMatchers("/", "/details/*", "/searching", "/search").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/details/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/addnew/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/login").defaultSuccessUrl("/").permitAll()
-                .and().logout().logoutSuccessUrl("/");
-
-
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+                .formLogin().loginPage("/login").permitAll()
+                .loginProcessingUrl("/authenticateTheUser").defaultSuccessUrl("/")
+                .and().logout().logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
     }
 }
---
+++ src/main/java/development/bulletinboard/controller/ExceptionController.java
@@ -0,0 +1,14 @@
+package development.bulletinboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ExceptionController {
+
+    @RequestMapping(value = "/access-denied", method = RequestMethod.GET)
+    public String accessDenied() {
+        return "access-denied";
+    }
+}
